3cbc1d91159e3fc2b2ac635f321e9f53aeb6bd02
SapnuPuas1018 <yuvalhayun20077@gmail.com>
2024-02-21 18:33:34 +0200

    “The best thing about being dead is that you don't know about it. It's like being stupid - it's only painful for others.”

--- Client.py
+++ Client.py
@@ -1,3 +1,8 @@
+"""
+author - Yuval Hayun
+date   - 21/02/2024
+"""
+
 import socket
 from tqdm import tqdm
 import concurrent.futures
@@ -7,6 +12,17 @@ IP = '127.0.0.1'
 
 
 def scan_port(port):
+    """
+    checks if a specific port on the specified IP address is open.
+    if the port is open then it prints the port's name
+
+    :param port: The port number to be scanned.
+    :type port: int
+
+    :return: None
+    :rtype: None
+
+    """
     try:
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         my_socket.settimeout(0.5)
@@ -20,6 +36,9 @@ def scan_port(port):
 
 
 def main():
+    """
+    main Function
+    """
     with concurrent.futures.ThreadPoolExecutor(max_workers=100) as runner:
         runners_list = []
         for port in tqdm(range(20, 1025)):
